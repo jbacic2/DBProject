67b88ee4a77cb0d9c4a7d1fef14e8d60c24c87d2
Derek <derek@llamabagel.ca>
2020-04-11 23:26:05 -0400

    Add reports to owner interface

--- owner/src/main/kotlin/comp/databases/project/owner/books/data/DummyRepository.kt
+++ owner/src/main/kotlin/comp/databases/project/owner/books/data/DummyRepository.kt
@@ -1,6 +1,7 @@
 package comp.databases.project.owner.books.data
 
 import comp.databases.project.shared.books.model.BookDetail
+import comp.databases.project.shared.reports.model.Report
 
 object DummyRepository : ManagementRepository {
     override fun addBook(book: BookDetail): Boolean {
@@ -11,5 +12,29 @@ object DummyRepository : ManagementRepository {
     override fun removeBook(isbn: String): Boolean {
         println("Remove $isbn")
         return true
+    }
+
+    override fun getReport(month: Int, year: Int): Report {
+        return Report(
+            month, year, listOf(
+                Report.LineItem("Sales", 9999.0),
+                Report.LineItem("Expenses", -4555.24)
+            ), listOf(
+                Report.Category(
+                    "Genre", listOf(
+                        Report.LineItem("Calendars", 8888.0),
+                        Report.LineItem("Magic", 10.0),
+                        Report.LineItem("Sci-Fi", 8.0)
+                    )
+                ),
+                Report.Category(
+                    "Author", listOf(
+                        Report.LineItem("J. K. Rowling", 1_000_000.0),
+                        Report.LineItem("J.-R. Sack", 15.99),
+                        Report.LineItem("J. R. R. Tolkien", 10.0)
+                    )
+                )
+            )
+        )
     }
 }
--- owner/src/main/kotlin/comp/databases/project/owner/books/data/ManagementRepository.kt
+++ owner/src/main/kotlin/comp/databases/project/owner/books/data/ManagementRepository.kt
@@ -1,9 +1,12 @@
 package comp.databases.project.owner.books.data
 
 import comp.databases.project.shared.books.model.BookDetail
+import comp.databases.project.shared.reports.model.Report
 
 interface ManagementRepository {
     fun addBook(book: BookDetail): Boolean
 
     fun removeBook(isbn: String): Boolean
+
+    fun getReport(month: Int, year: Int): Report
 }
